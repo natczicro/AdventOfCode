85a46ba1ef07fd2c0c9afdf90a08a968a937411e
natczicro <9286877+natczicro@users.noreply.github.com>
2023-12-07 21:01:34 +0000

    Change logic to use np arrays

--- adventOfCode2023/Day7/daySeven2023.py
+++ adventOfCode2023/Day7/daySeven2023.py
@@ -5,68 +5,65 @@ linesFromFile = file1.readlines()
 
 def determineTypeOfHand(hand):
   cardCount = Counter(hand)
-
+  pairOfCards = None
+  threeOfAKind= None
   cardCount[list(cardCount.keys())[0]]
   for count, keys in enumerate(cardCount):
+
     #print(cardCount[keys])
     if cardCount[keys] == 5:
-      rank = 1
+      rank = 7
       return rank
     elif cardCount[keys] == 4:
-      rank = 2
+      rank = 6
       return rank
     elif cardCount[keys] == 3:
-      if cardCount[list(cardCount.keys())[count+1]] == 2:
-        rank = 3
-      else:
-        rank = 4
-      return rank
-    elif cardCount[keys] ==2:
-      if cardCount[list(cardCount.keys())[count+1]] == 3:
-        rank = 3
-        return rank
-      elif (cardCount[list(cardCount.keys())[count+1]] == 2) or \
-      (cardCount[list(cardCount.keys())[count+2]]== 2):
+      if pairOfCards:
         rank = 5
         return rank
-      else:
-        rank = 6
+      threeOfAKind = True
+    elif cardCount[keys] ==2:
+      if threeOfAKind:
+        rank = 5
         return rank
-  rank = 7
+      if pairOfCards:
+        rank = 3
+        return rank
+      pairOfCards = True
+
+  if threeOfAKind:
+    rank = 4
+    return rank
+  if pairOfCards:
+    rank = 2
+    return rank
+  rank = 1
   return rank
-    
 
-  
-  print(len(cardCount))
-  print(cardCount)
+def separateHandIntoArray(hand,value,type):
+  #print(list(hand))
+  bid,_ = value.split('\n')
 
-testhand = list('11111')
-print('five of a kind has rank: ',determineTypeOfHand(testhand))
-testhand = list('AAAA5')
-print('four of a kind has rank: ',determineTypeOfHand(testhand))
+  newList = [type, list(hand)[0], 
+             list(hand)[1], list(hand)[2],
+             list(hand)[3], list(hand)[4], int(bid)]
 
-testhand = list('22333')
-print('full house has rank: ',determineTypeOfHand(testhand))
+  print(newList)
 
-testhand = list('76555')
-print('three of a kind has rank: ',determineTypeOfHand(testhand))
+exitCount = 0
+category = {'five':[],
+   'four':[],
+   'full':[],
+   'three':[],
+   'twopair':[],
+   'pair':[],
+   'high':[]}
 
-testhand = list('JJQQT')
-print('two pair has rank: ',determineTypeOfHand(testhand))
-
-testhand = list('44TJK')
-print('a pair has rank: ',determineTypeOfHand(testhand))
-
-testhand = list('12345')
-print('nothing has rank: ',determineTypeOfHand(testhand))
-
-
-
-
-'''
 for lines in linesFromFile:
   hand,value = lines.split(' ')
-  print(hand, value)
-  determineTypeOfHand(hand)
-  break
-'''
+  rank = determineTypeOfHand(hand)
+  separateHandIntoArray(hand,value,rank)
+
+  exitCount += 1
+  if exitCount > 20:
+    break
