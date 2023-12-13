1286ff9f1f8fd511e651b8ffea55590f50624ab1
natczicro <9286877+natczicro@users.noreply.github.com>
2023-12-13 04:39:12 +0000

    Part One Solution

--- adventOfCode2023/Day11/dayEleven2023.py
+++ adventOfCode2023/Day11/dayEleven2023.py
@@ -1,7 +1,8 @@
-file1 = open('inputTest', 'r')
+file1 = open('input', 'r')
 linesFromFile = file1.readlines()
 import re
 import numpy as np
+import itertools
 
 newList = list(map(lambda x: x.replace('.', '0'), linesFromFile))
 
@@ -15,7 +16,7 @@ for rowNumber, items in enumerate(newList):
   try:
     splitText,_ = items.split('\n')
   except:
-    print("end of the stupid file")
+    splitText = items
   #print(splitText)
   splitText= re.sub(r'#', '1', splitText)
   newListAgain.append([*splitText])
@@ -24,18 +25,52 @@ for rowNumber, items in enumerate(newList):
 #newListAgain = [val for sublist in newListAgain for val in sublist]
 testArray = np.array(newListAgain,dtype='object')
 
-rowsToAdd = []
-columnsToAdd = []
-
 testArray[testArray == '0'] = 0
 testArray[testArray == '1'] = 1
+#print(testArray)
+#print(testArray.shape)
 
-print("Zero rows")
-zeroRows = (np.count_nonzero(testArray, axis = 0))
-print("Zero columns")
-zeroColumns = (np.count_nonzero(testArray, axis = 1))
+#print("Zero columns")
+zeroColumns = (np.count_nonzero(testArray, axis = 0))
+#print(zeroColumns)
+columnsToAdd = []
+for columns,items in enumerate(zeroColumns):
+  if items == 0:
+    #print("inserting column at", columns)
+    columnsToAdd.append(columns)
+#print(columnsToAdd)
+for count, items in enumerate(columnsToAdd):
+  #print(items)
+  testArray=np.insert(testArray, items+count, 0, axis=1)
 
+#print("Zero rows")
+zeroRows = (np.count_nonzero(testArray, axis = 1))
+#print(zeroRows)
+rowsToAdd = []
+for rows,items in enumerate(zeroRows):
+  if items == 0:
+    #print("inserting row at", rows)
+    rowsToAdd.append(rows)
+#print(rowsToAdd)
 
-print(np.nonzero(testArray))
-print(testArray)
-print(testArray.shape)
+for count, items in enumerate(rowsToAdd):
+  testArray=np.insert(testArray, items+count, 0, axis=0)
+
+#print(np.nonzero(testArray))
+#print(testArray)
+#print(testArray.shape)
+galaxyCoords = (np.nonzero(testArray))
+galaxyCoords = np.array((galaxyCoords[0], galaxyCoords[1])).T
+#print(galaxyCoords)
+
+allPairs = itertools.combinations(galaxyCoords, 2)
+
+totalShortestLength = 0
+for pairs in allPairs:
+  #print(pairs)
+  #print(pairs[0][0])
+  shortestLength = abs(pairs[0][0] - pairs[1][0]) + abs(pairs[0][1] - pairs[1][1])
+  #print(shortestLength)
+  totalShortestLength += shortestLength
+
+print(totalShortestLength)
