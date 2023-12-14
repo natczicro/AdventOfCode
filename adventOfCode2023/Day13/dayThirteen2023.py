403bd79d38a3a78bf4ab0bab40c0b0b390adfcd1
natczicro <9286877+natczicro@users.noreply.github.com>
2023-12-14 04:40:22 +0000

    Progress...maybe

--- adventOfCode2023/Day13/dayThirteen2023.py
+++ adventOfCode2023/Day13/dayThirteen2023.py
@@ -6,79 +6,95 @@ linesFromFile = file1.readlines()
 #print(linesFromFile)
 
 
-def determineSymmetry(patternList):
-  patternArray = np.array(patternList)
-  previousRow = []
-  for rowNumber, row in enumerate(patternArray):
-    #print(patternArray[rowNumber])
-    #input()
-    #print(previousRow))
-    #print(row))
-    if np.array_equal(patternArray[rowNumber], patternArray[rowNumber - 1]):
-      if rowNumber - 2 >= 0 and rowNumber + 1 < patternArray.shape[0]:
-        if np.array_equal(patternArray[rowNumber + 1],
-                          patternArray[rowNumber - 2]):
-          symmetryRow = [rowNumber]
-          axisSymmetry = 0
-          return symmetryRow, axisSymmetry
-        else:
-          continue
-      else:
-        symmetryRow = [rowNumber]
-        axisSymmetry = 0
-        return symmetryRow, axisSymmetry
-
-
-  patternArray = np.array(patternList).T
+def determineSymmetry(patternArray):
+  symmetryFlag = False
   for rowNumber, row in enumerate(patternArray):
     #print(previousRow)
-    #print(row))
+    if rowNumber ==0:
+      continue
+    print(row)
     if np.array_equal(patternArray[rowNumber], patternArray[rowNumber - 1]):
-      if rowNumber - 2 >= 0 and rowNumber + 1 < patternArray.shape[0]:
-        if np.array_equal(patternArray[rowNumber + 1],
-                          patternArray[rowNumber - 2]):
-          symmetryRow = [rowNumber]
-          axisSymmetry = 1
-          return symmetryRow, axisSymmetry
+      print("matching rows")
+      count=1
+      symmetryFlag = True
+      symmetryRow = [rowNumber]
+      while (rowNumber - 1-count >= 0 and rowNumber + count < patternArray.shape[0]):
+        print("checking other lines for sym")
+        if np.array_equal(patternArray[rowNumber +count],
+                          patternArray[rowNumber -1-count]):
+          symmetryFlag = True
         else:
-          continue
-      else:
-        symmetryRow = [rowNumber]
-        axisSymmetry = 1
-        return symmetryRow, axisSymmetry
+          symmetryFlag = False
+          print("mismatch in symm")
+        count += 1
+  if symmetryFlag:
+    return symmetryRow
+
 
 
 def determineSummary(symmetryIndex, axis):
   if axis == 0:
     #calculate for rows
     summarization = int(symmetryIndex[0]) * 100
-  else:
+  elif axis ==1:
     summarization = int(symmetryIndex[0])
   #print(summarization)
   return summarization
 
+#def doABunchOfStuff():
+  
 
 def partOne():
   patternList = []
   summary = 0
   for rowNumber, items in enumerate(linesFromFile):
+    #print(items)
     if items == '\n':  #Different pattern
+      symmetryIndex = None
       patternArray = np.array(patternList)
       print(patternArray)
-      symmetryIndex, axis = determineSymmetry(patternList)
-      print(symmetryIndex, axis)
-      summary += determineSummary(symmetryIndex, axis)
-      print(summary)
-      patternList = []
       #input()
+      print("checking rows")
+      symmetryIndex = determineSymmetry(patternArray)
+      if symmetryIndex is not None:
+        print("symmetry row is ", symmetryIndex)
+        axis = 0
+        summary += determineSummary(symmetryIndex, axis)
+      else:
+        print("checking columns")
+        patternArray = np.array(patternList).T
+        symmetryIndex = determineSymmetry(patternArray)
+        if symmetryIndex is not None:
+          print("symmetry column is ", symmetryIndex)
+          axis = 1
+          summary += determineSummary(symmetryIndex, axis)
+      patternList = []
+      print(summary)
       continue
-    try:
+    if items.endswith('\n'):
       splitText, _ = items.split('\n')
       patternList.append([*splitText])
-      print(splitText)
-    except:
-      "end of file"
-
+    else:
+      symmetryIndex = None
+      patternList.append([*items])
+      patternArray = np.array(patternList)
+      print(patternArray)
+      #input()
+      print("checking rows")
+      symmetryIndex = determineSymmetry(patternArray)
+      if symmetryIndex is not None:
+        axis = 0
+        summary += determineSummary(symmetryIndex, axis)
+      else:
+        print("checking columns")
+        patternArray = np.array(patternList).T
+        symmetryIndex = determineSymmetry(patternArray)
+        if symmetryIndex is not None:
+          axis = 1
+          summary += determineSummary(symmetryIndex, axis)
+      print(summary)
+      patternList = []
+      continue
   print(summary)
 
 
