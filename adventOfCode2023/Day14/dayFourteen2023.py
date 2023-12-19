c6fd0185dabfb5648adce083d06ba73e6ce65c09
natczicro <9286877+natczicro@users.noreply.github.com>
2023-12-19 00:29:15 +0000

    Day 14 part one and some attempted restructuring to use replit properly

--- adventOfCode2023/Day14/dayFourteen2023.py
+++ adventOfCode2023/Day14/dayFourteen2023.py
@@ -2,14 +2,63 @@ file1 = open('input', 'r')
 linesFromFile = file1.readlines()
 
 import numpy as np
+import re
 
-#Split list into inidivual elements
+def findCubeRocks(line):
+  #print(line)
+  indices = []
+  for match in re.finditer(r'\#', line):
+    indices.append(match.start())
+  return indices
 
-#Assign starting blocker point as 0
+def moveRoundRocks(line,blockRow,numberOfRows,load):
+  indices = []
+  for match in re.finditer(r'O', line):
+    indices.append(match.start())
+  for index in indices:
+    load += numberOfRows - blockRow[index] 
+    blockRow[index]+=1
+  return blockRow, load
+    
+  
+def moveBlock(blockRow, line,row,numberOfRows, load):
+  cubeRocks = findCubeRocks(line)
+  blockRow, load = moveRoundRocks(line,blockRow,numberOfRows,load)
+  print(cubeRocks)
+  #print(blockRow)
+  print(line)
+  for index in cubeRocks:
+    print("have cube rocks")
+    blockRow[index] = row+1
+  print(blockRow)
+  print(load)
+  #input()
+  return blockRow, load
+  
+def partOne():
+  #Split list into inidivual elements
+  numberOfRows = len(linesFromFile)
+  splitList = []
+  for lines in linesFromFile:
+    lines,_ = lines.split('\n')
+    splitList.append(lines)
+  
+  #array=np.array(splitList)
+  #print(array.shape)
+  print(len(splitList[0]))
+  
+  #Assign starting blocker point as 0
+  blockRow = [0]*len(splitList[0])
+  print(blockRow)
+  
+  #loop through array rows
+  #For each 0 see where it moves to (check blocker point) and calculate score
+  #Move blocking point if 0 gets moved or if # is present
+  load = 0
+  for row, line in enumerate(splitList):
+    blockRow, load = moveBlock(blockRow,line,row,numberOfRows,load)
+    
+  print(load)
+  #rinse and repeat
 
-#loop through array rows
-#For each 0 see where it moves to (check blocker point) and calculate score
-#Move blocking point if 0 gets moved or if # is present
-
-#rinse and repeat
-
+partOne()
---
+++ main.py
@@ -0,0 +1,5 @@
+import adventOfCode2023
+
+print("Hello World")
+
+adventOfCode2023.Day14.dayFourteen2023.partOne()
