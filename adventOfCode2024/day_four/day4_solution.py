a2fcfc2b4d157c21382376c5b0b36faaeeac6e18
nccrooks <nczicro@gmail.com>
2024-12-04 23:12:31 -0500

    part one solution day 4

--- adventOfCode2024/day_four/day4_solution.py
+++ adventOfCode2024/day_four/day4_solution.py
@@ -1,7 +1,7 @@
 import re
 import numpy as np
 
-file1 = open('sample.txt', 'r')
+file1 = open('day_three_input.txt', 'r')
 linesFromFile = file1.readlines()
 
 big_list = []
@@ -13,52 +13,52 @@ for line in linesFromFile:
     line_entry = list(line)
     big_list.append(line_entry)
 
-print(big_list)
+#print(big_list)
 
 numpy_array = np.array(big_list)
 
-print(numpy_array)
+#print(numpy_array)
 
 x_locations=(np.where(numpy_array=='X'))
 
-print(x_locations[0][0],x_locations[1][0])
+#print(x_locations[0][0],x_locations[1][0])
 
-input()
-print(np.size(x_locations))
+#input()
+#print(np.size(x_locations))
 
 xes_present = int(np.size(x_locations)/2)
 
-print(xes_present)
 
 def search_around_x(start_x, start_y, y_direction, x_direction,numpy_array):
-    if start_y - y_direction*3 <0:
+    if start_y + y_direction*3<0:
         return False
-    elif start_y + y_direction*3>np.shape(numpy_array)[0]:
+    elif start_y + y_direction*3>=np.shape(numpy_array)[0]:
         return False
-    elif start_x - x_direction*3 <0:
+    elif start_x + x_direction*3<0:
         return False
-    elif start_x + x_direction*3 > np.shape(numpy_array)[1]:
+    elif start_x + x_direction*3>= np.shape(numpy_array)[1]:
         return False
     
-    if numpy_array[start_x+x_direction][start_y+y_direction] == 'M' and numpy_array[start_x+x_direction*2][start_y+y_direction*2] == 'A' and numpy_array[start_x+x_direction*3][start_y+y_direction*3]:
+    if numpy_array[start_x+x_direction][start_y+y_direction] == 'M' and numpy_array[start_x+x_direction*2][start_y+y_direction*2] == 'A' and numpy_array[start_x+x_direction*3][start_y+y_direction*3]=='S':
         return True
     else:
         return False
 
-print(np.shape(numpy_array)[0])
+#print(np.shape(numpy_array)[0])
 
 total = 0
 for i in range(xes_present):
-    print(x_locations[0][i],x_locations[1][i])
+    #print(x_locations[0][i],x_locations[1][i])
+
     total+=search_around_x(x_locations[0][i], x_locations[1][i], 1, 1,numpy_array)
     total+=search_around_x(x_locations[0][i], x_locations[1][i], 0, 1,numpy_array)
     total+=search_around_x(x_locations[0][i], x_locations[1][i], 1, 0,numpy_array)
     total+=search_around_x(x_locations[0][i], x_locations[1][i], 1, -1,numpy_array)
     total+=search_around_x(x_locations[0][i], x_locations[1][i], -1, 1,numpy_array)
-    total+=search_around_x(x_locations[0][i], x_locations[1][i], -1, 1,numpy_array)
-    total+=search_around_x(x_locations[0][i], x_locations[1][i], 1, -1,numpy_array)
+    total+=search_around_x(x_locations[0][i], x_locations[1][i], -1, 0,numpy_array)
+    total+=search_around_x(x_locations[0][i], x_locations[1][i], 0, -1,numpy_array)
     total+=search_around_x(x_locations[0][i], x_locations[1][i], -1, -1,numpy_array)
-
-    input()
+    #print(total)
+    #input()
 
 print(total)
