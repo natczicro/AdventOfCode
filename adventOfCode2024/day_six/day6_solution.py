15491af394dd29591bba0b869c99b66f280cec34
natczicro <nczicro@gmail.com>
2024-12-11 22:36:02 -0500

    same as last commit removed useless stuff day 6

--- adventOfCode2024/day_six/day6_solution.py
+++ adventOfCode2024/day_six/day6_solution.py
@@ -54,6 +54,7 @@ def x_es_created(start, end):
             for i in range(start[0]-end[0]):
                 list_to_return.append(f'{start[0]-i}{start[1]}')
     return list_to_return
+
 def distance_travelled(starting_point, maze_path, direction):
     if direction == 'up':
         start = starting_point[0]
@@ -62,13 +63,11 @@ def distance_travelled(starting_point, maze_path, direction):
         if len(np.where(possible_ends <start)[0]) ==0:
             #guard is leaving
             next_direction='done'
-            distinct_positions = start
-            return np.array([0,starting_point[1]]),distinct_positions,next_direction
+            return np.array([0,starting_point[1]]),next_direction
 
         end_index = np.where(possible_ends <start)[0][-1]
         end = possible_ends[end_index]
 
-        distinct_positions = abs(start-end)-1
         next_direction='right'
         end_point=np.array([end+1,starting_point[1]])
 
@@ -79,11 +78,9 @@ def distance_travelled(starting_point, maze_path, direction):
         if len(np.where(possible_ends >start)[0]) ==0:
             #guard is leaving
             next_direction='done'
-            distinct_positions = abs(start-len(maze_path))
-            return np.array([len(maze_path),starting_point[1]]),distinct_positions,next_direction
+            return np.array([len(maze_path),starting_point[1]]),next_direction
         end_index = np.where(possible_ends > start)[0][0]
         end = possible_ends[end_index]
-        distinct_positions = abs(start-end)-1
         next_direction='left'
         end_point=np.array([end-1,starting_point[1]])
       
@@ -95,12 +92,10 @@ def distance_travelled(starting_point, maze_path, direction):
         if len(np.where(possible_ends >start)[0]) ==0:
             #guard is leaving
             next_direction='done'
-            distinct_positions = abs(start-len(maze_path))
-            return np.array([starting_point[0],len(maze_path)]),distinct_positions,next_direction
+            return np.array([starting_point[0],len(maze_path)]),next_direction
         
         end_index = np.where(possible_ends > start)[0][0]
         end = possible_ends[end_index]
-        distinct_positions = abs(start-end)-1
         next_direction='down'
         end_point=np.array([starting_point[0],end-1])
 
@@ -112,19 +107,16 @@ def distance_travelled(starting_point, maze_path, direction):
         if len(np.where(possible_ends <start)[0]) ==0:
             #guard is leaving
             next_direction='done'
-            distinct_positions = start
-            return np.array([starting_point[0],0]),distinct_positions,next_direction
+            return np.array([starting_point[0],0]),next_direction
         
         end_index = np.where(possible_ends <start)[0][-1]
         end = possible_ends[end_index]
-
-        distinct_positions = abs(start-end)-1
         next_direction='up'
         end_point=np.array([starting_point[0],end+1])
 
     
     
-    return end_point,distinct_positions,next_direction
+    return end_point,next_direction
 
 print(maze_array)
 
@@ -135,17 +127,14 @@ direction = 'up'
 print(starting_point[0],starting_point[1])
 #maze_path = get_array_slice(starting_point, maze_array, direction)
 #distance = distance_travelled(starting_point, maze_path, direction)
-total_distance=0
 
 distinct_spots_list = []
 while direction is not 'done':
     maze_path = get_array_slice(starting_point, maze_array, direction)
     old_start = starting_point
-    starting_point,distance,direction = distance_travelled(starting_point, maze_path, direction)
+    starting_point,direction = distance_travelled(starting_point, maze_path, direction)
     distinct_spots_list.append(x_es_created(old_start, starting_point))
     print(starting_point)
-    total_distance+=distance
-print(total_distance)
 flat_list = []
 
 for xs in distinct_spots_list:
