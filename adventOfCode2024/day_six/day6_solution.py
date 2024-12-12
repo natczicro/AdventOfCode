adebbf56d92c59347054efe53634edfa3d34222a
natczicro <nczicro@gmail.com>
2024-12-11 23:57:46 -0500

    total rework day 6, works part one still fails part 2...

--- adventOfCode2024/day_six/day6_solution.py
+++ adventOfCode2024/day_six/day6_solution.py
@@ -25,4 +25,32 @@ maze_array = np.array(big_list)
 
 starting_point = np.where(maze_array=='^')
 starting_point=np.array([starting_point[0][0],starting_point[1][0]])
-direction = 'up'
+
+def step_forward_one(starting_point,maze_array):
+    #returns a new point, and an updated maze_array
+    
+    #print(maze_array)
+    if maze_array[starting_point[0]-1,starting_point[1]] == '#':
+        maze_array=np.rot90(maze_array)
+        return maze_array, True
+    elif starting_point[0]-1<0:
+        return maze_array, False
+    maze_array[starting_point[0],starting_point[1]]='X'
+    maze_array[starting_point[0]-1,starting_point[1]]='^'
+
+
+    return maze_array, True
+
+in_maze = True
+next_step = starting_point
+while in_maze == True:
+    starting_point = np.where(maze_array=='^')
+    starting_point=np.array([starting_point[0][0],starting_point[1][0]])
+    maze_array, in_maze = step_forward_one(starting_point,maze_array)
+    print(maze_array)
+    print("next one")
+
+print(np.count_nonzero(maze_array=='X'))
+print(maze_array)
+
+        
